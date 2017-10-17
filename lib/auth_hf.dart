34906a2157345749e58eb868cf5c8fcc9db75d7b
Tobe O <thosakwe@gmail.com>
2017-10-17 16:05:15 -0400

    bye static

--- lib/auth_hf.dart
+++ lib/auth_hf.dart
@@ -80,15 +80,11 @@ Future configureServer(Angel app) async {
   await app.configure(oauth2.configureServer);
 
   var webRoot = fs.directory('web');
-  VirtualDirectory vDir;
 
-  if (app.isProduction) {
-    vDir = new CachingVirtualDirectory(app, fs, source: webRoot);
-  } else {
-    vDir = new VirtualDirectory(app, fs, source: webRoot);
+  if (!app.isProduction) {
+    var vDir = new VirtualDirectory(app, fs, source: webRoot);
+    app.use(vDir.handleRequest);
   }
-
-  app.use(vDir.handleRequest);
 
   app.use((RequestContext req) {
     throw new AngelHttpException.notFound(
