1f422f41a1bcf246a66324c4055f0fdfed145a31
Tobe O <thosakwe@gmail.com>
2017-10-18 00:49:58 -0400

    Footer+ads

--- views/layout.jl
+++ views/layout.jl
@@ -13,15 +13,32 @@
 </head>
 <body>
     <block name="body"></block>
-    <div class="level is-mobile">
+    <div class="level">
         <div class="level-item">
             <ins class="adsbygoogle"
-                 style="display:block"
+                 style="display:inline-block;width:728px;height:90px"
                  data-ad-client="ca-pub-2116133816688639"
-                 data-ad-slot="2103896424"
-                 data-ad-format="auto"></ins>
+                 data-ad-slot="9060681851"></ins>
+        </div>
+        <div class="level-item">
+            <ins class="adsbygoogle"
+                 style="display:inline-block;width:728px;height:90px"
+                 data-ad-client="ca-pub-2116133816688639"
+                 data-ad-slot="9060681851"></ins>
         </div>
     </div>
+    <section class="hero is-dark is-bold">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h2 class="subtitle">
+                    <a href="https://github.com/auth-hf/auth-hf" target="_blank">
+                        <i class="fa fa-github"></i>
+                        &nbsp; Fork on Github
+                    </a>
+                </h2>
+            </div>
+        </div>
+    </section>
     <script src="/bower_components/jquery/dist/jquery.min.js"></script>
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-68026607-4"></script>
